cbbfc6c2ee45ff905ad3c94e9c3fc427c3a8e70b
daviiaex <116956288+daviiaex@users.noreply.github.com>
2022-12-15 20:04:25 +0300

    first implementation

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,10 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("cloud not read a file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
